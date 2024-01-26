439c0ee19fcadcec3af8a27f35c28b5db0374df8
averybrubeck <averybrubeck@yahoo.com>
2024-01-25 20:41:51 -0500

    added ptq and pbq methods

--- linked_lists.rb
+++ linked_lists.rb
@@ -1,10 +1,41 @@
-#linked lists
+# linked lists
 
+# List Entry
 class Entry
   attr_accessor :next, :data
-  
+
   def initialize(data)
     @next = nil
     @data = data
+  end
+end
+
+# List
+class List
+  attr_accessor :name
+
+  def initialize
+    @head = nil
+    @tail = nil
+  end
+
+  def ptq(entry)
+    if @head.nil?
+      @head = entry
+      @tail = entry
+    else
+      entry.next = @head
+      @head = entry
+    end
+  end
+
+  def pbq(entry)
+    if @head.nil?
+      @head = entry
+      @tail = entry
+    else
+      @tail.next = entry
+      @tail = entry
+    end
   end
 end
